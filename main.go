a442bf9d39be229f7269b9f40dad4953df2a945c
Mark Witt <mwitt@newstore.com>
2023-12-05 19:55:46 +0100

    .concopyuse

--- main.go
+++ main.go
@@ -14,12 +14,18 @@ import (
 )
 
 func main() {
-	flag.Parse()
-	patterns := flag.Args()
-
+	// Use patterns from .concopyuse if it exists, otherwise from command-line arguments
+	patterns, err := readConcopyUsePatterns(".")
+	if err != nil {
+		fmt.Println("Error reading .concopyuse:", err)
+		return
+	}
 	if len(patterns) == 0 {
-		// If no pattern is provided, default to "."
-		patterns = append(patterns, ".")
+		flag.Parse()
+		patterns = flag.Args()
+		if len(patterns) == 0 {
+			patterns = append(patterns, ".") // Default to "."
+		}
 	}
 
 	if err := concopy(".", patterns); err != nil {
@@ -28,30 +34,37 @@ func main() {
 }
 
 func concopy(rootDir string, patterns []string) error {
-	gitIgnore, err := gitignore.CompileIgnoreFile(filepath.Join(rootDir, ".gitignore"))
-	if err != nil {
-		return err
-	}
-
 	var buffer bytes.Buffer
-	err = filepath.Walk(rootDir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if info.IsDir() || gitIgnore.MatchesPath(path) || !shouldInclude(path, patterns, rootDir) || strings.Contains(path, "/.git/") || strings.HasSuffix(path, "/.git") {
-			return nil
-		}
-
-		data, err := ioutil.ReadFile(path)
+	err := filepath.Walk(rootDir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 
-		fmt.Println(path)
-		buffer.WriteString(path + ":\n")
-		buffer.Write(data)
-		buffer.WriteString("\n\n")
+		// Skip the .git directory
+		if info.IsDir() && (info.Name() == ".git" || strings.Contains(path, "/.git/")) {
+			return filepath.SkipDir
+		}
 
+		if info.IsDir() {
+			gitIgnore, err := loadGitIgnore(path)
+			if err != nil {
+				fmt.Println("Error loading .gitignore:", err)
+				return nil
+			}
+			if gitIgnore != nil && gitIgnore.MatchesPath(path) {
+				return filepath.SkipDir
+			}
+		} else if shouldInclude(path, patterns, rootDir) && !isIgnoredByGitignore(path, rootDir) {
+			data, err := ioutil.ReadFile(path)
+			if err != nil {
+				return err
+			}
+
+			fmt.Println(path)
+			buffer.WriteString(path + ":\n")
+			buffer.Write(data)
+			buffer.WriteString("\n\n")
+		}
 		return nil
 	})
 
@@ -60,6 +73,24 @@ func concopy(rootDir string, patterns []string) error {
 	}
 
 	return clipboard.WriteAll(buffer.String())
+}
+
+func loadGitIgnore(dir string) (*gitignore.GitIgnore, error) {
+	gitignorePath := filepath.Join(dir, ".gitignore")
+	if _, err := os.Stat(gitignorePath); os.IsNotExist(err) {
+		return nil, nil
+	}
+	return gitignore.CompileIgnoreFile(gitignorePath)
+}
+
+func isIgnoredByGitignore(filePath, rootDir string) bool {
+	for dir := filePath; dir != rootDir; dir = filepath.Dir(dir) {
+		gitIgnore, _ := loadGitIgnore(filepath.Dir(dir))
+		if gitIgnore != nil && gitIgnore.MatchesPath(filePath) {
+			return true
+		}
+	}
+	return false
 }
 
 func shouldInclude(filePath string, patterns []string, rootDir string) bool {
@@ -96,4 +127,22 @@ func shouldInclude(filePath string, patterns []string, rootDir string) bool {
 		}
 	}
 	return false
+}
+
+// readConcopyUsePatterns reads the .concopyuse file and returns the patterns
+func readConcopyUsePatterns(rootDir string) ([]string, error) {
+	concopyUsePath := filepath.Join(rootDir, ".concopyuse")
+	content, err := ioutil.ReadFile(concopyUsePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, nil // No .concopyuse file, not an error
+		}
+		return nil, err
+	}
+
+	patterns := strings.Split(strings.TrimSpace(string(content)), "\n")
+	for i, pattern := range patterns {
+		patterns[i] = strings.TrimSpace(pattern)
+	}
+	return patterns, nil
 }
